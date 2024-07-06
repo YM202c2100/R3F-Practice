8f9de9f73a4591734b2dd1b2ef7f1e5d31729fb1
YM202c2100 <y.m202c2100@gmail.com>
2024-07-06 15:40:13 +0900

    [style] AboutR3Fの文字の左端と画像の左端が一致しなかったため、左マージンを削除
    変更前のようにmd以上で画像と文字が被るように文字を左にずらした

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -4,7 +4,7 @@ function MainTitle(){
     <div className="relative">
       <h1 
         className="
-          absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:translate-x-0
+          absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:-translate-x-10
           font-bold text-[#ed6969] text-5xl lg:text-6xl
           text-center"
       >
@@ -14,7 +14,7 @@ function MainTitle(){
         src="GeometryImage.png"
         className="
           w-full md:w-1/2
-          md:ml-10 md:mt-10
+          md:mt-10
           md:rounded-3xl"
       />
     </div>
