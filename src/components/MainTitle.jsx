502fd4f1b87fc7be7393fb277e29f7ac3cd80321
YM202c2100 <y.m202c2100@gmail.com>
2024-07-06 14:11:14 +0900

    [style] 文字の大きさを拡大

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -4,7 +4,8 @@ function MainTitle(){
     <div className="relative">
       <h1 
         className="
-          absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+          text-5xl"
       >
         Practice for R3F
       </h1>
