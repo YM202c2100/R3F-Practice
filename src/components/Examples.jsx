50d82b0cb58e519804402478cc2856f149a248cd
YM202c2100 <y.m202c2100@gmail.com>
2024-07-06 22:56:45 +0900

    [feat] Examplesセクションタイトルを追加

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,13 @@
 function Examples(){
   return(
     <div>
+      <h2 
+        className="
+          text-center
+          text-5xl font-bold text-[#72c371]"
+      >
+        Examples
+      </h2>
       <div className="border-4 border-[#97e396]">
         example
       </div>
