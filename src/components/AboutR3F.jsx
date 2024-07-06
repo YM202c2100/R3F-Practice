1bf4823876392189403229ee648e864236955314
YM202c2100 <y.m202c2100@gmail.com>
2024-07-06 15:30:32 +0900

    [style] セクションタイトルのテキストサイズ・色・太さを変更

--- src/components/AboutR3F.jsx
+++ src/components/AboutR3F.jsx
@@ -2,7 +2,7 @@
 function AboutR3F(){
   return(
     <div>
-      <h2>React-three-fiber(R3F)とは</h2>
+      <h2 className="text-5xl font-bold text-[#4f5584] ">React-three-fiber(R3F)とは</h2>
       <p>
         webサイト上で3Dモデルを簡単に扱うためのJavaScriptライブラリのひとつにThree.jsがある。React-three-fiberは、そのthree.jsをReactでより簡単に使用できるようにしたReactレンダラーである。
       </p>
