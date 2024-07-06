652a28407faf4819380aa993eb3e1d8264894427
YM202c2100 <y.m202c2100@gmail.com>
2024-07-06 17:04:31 +0900

    [feat] メッシュが回転し続けるように変更

--- src/components/RotationMesh.jsx
+++ src/components/RotationMesh.jsx
@@ -1,8 +1,18 @@
-
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
 
 function RotationMesh(){
+  const meshRef = useRef()
+  useFrame((state, delta)=>{
+    if(meshRef.current){
+      meshRef.current.rotation.x += delta * 0.4
+      meshRef.current.rotation.y += delta * 0.5
+      meshRef.current.rotation.z += delta * 0.1
+    }
+  })
+
   return(
-    <mesh>
+    <mesh ref={meshRef}>
       <torusKnotGeometry/>
       <meshNormalMaterial/>
     </mesh>
