a781dbebe08ba3f8f3d15f06fa832ca76f531b2a
YM202c2100 <y.m202c2100@gmail.com>
2024-07-06 15:41:02 +0900

    [style] セクションタイトルを中央揃えに変更

--- src/components/AboutR3F.jsx
+++ src/components/AboutR3F.jsx
@@ -2,7 +2,11 @@
 function AboutR3F(){
   return(
     <div>
-      <h2 className="text-5xl font-bold text-[#4f5584] ">React-three-fiber(R3F)とは</h2>
+      <h2 
+        className="
+          text-5xl font-bold text-[#4f5584]
+          text-center
+        ">React-three-fiber(R3F) とは</h2>
       <p>
         webサイト上で3Dモデルを簡単に扱うためのJavaScriptライブラリのひとつにThree.jsがある。React-three-fiberは、そのthree.jsをReactでより簡単に使用できるようにしたReactレンダラーである。
       </p>
