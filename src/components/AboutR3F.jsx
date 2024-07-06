4c6a4ca141773cae9f3abdf076c658a64a43c9ce
YM202c2100 <y.m202c2100@gmail.com>
2024-07-06 15:55:56 +0900

    [refactor] 見やすいようにコードのインデントを調整

--- src/components/AboutR3F.jsx
+++ src/components/AboutR3F.jsx
@@ -4,10 +4,12 @@ function AboutR3F(){
     <div>
       <h2 
         className="
-mb-10
+          mb-10
           text-5xl font-bold text-[#4f5584]
-          text-center
-        ">React-three-fiber(R3F) とは</h2>
+          text-center"
+      >
+        React-three-fiber(R3F) とは
+      </h2>
       <p className="text-xl font-medium mx-5">
         webサイト上で3Dモデルを簡単に扱うためのJavaScriptライブラリのひとつにThree.jsがある。React-three-fiberは、そのthree.jsをReactでより簡単に使用できるようにしたReactレンダラーである。
       </p>
