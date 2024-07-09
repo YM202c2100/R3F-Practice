0defb8aec5ba0c3da2dc3c8fa14ee0ac4a6c0a0b
YM202c2100 <y.m202c2100@gmail.com>
2024-07-09 17:59:03 +0900

    [chore] uniform変数でない変数名を修正
    uniformのuを変数名から削除

--- src/shaders/sea/vertex.glsl
+++ src/shaders/sea/vertex.glsl
@@ -5,17 +5,17 @@ varying float vElevation;
 #include "./LygiaLibrary/genrative/cnoise.glsl"
 
 void main(){
-  float uWaveFreqX = 3.5;
-  float uWaveFreqZ = 2.0;
-  float uWaveElevation = 0.2;
+  float WaveFreqX = 3.5;
+  float WaveFreqZ = 2.0;
+  float WaveElevation = 0.2;
   float waveBumpyFreq = 3.5;
   float waveBumpySpeed = 0.5;
   float waveBumpyElevation = 0.15;
 
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  float elevation = sin(modelPosition.x * uWaveFreqX + uTime*0.8) *
-                    sin(modelPosition.z * uWaveFreqZ + uTime*0.5) *
-                    uWaveElevation;
+  float elevation = sin(modelPosition.x * WaveFreqX + uTime*0.8) *
+                    sin(modelPosition.z * WaveFreqZ + uTime*0.5) *
+                    WaveElevation;
   
   for(float i=1.0; i<4.0; i++){
     elevation -= abs(cnoise(vec3(modelPosition.xz * waveBumpyFreq * i, uTime * waveBumpySpeed)) * waveBumpyElevation / i);
