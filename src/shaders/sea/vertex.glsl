9c70a8d4577dda7d4bb4fb868c46732f2cef1111
YM202c2100 <y.m202c2100@gmail.com>
2024-07-09 17:48:01 +0900

    [feat] パーリンノイズを用いて波の表面に凹凸を追加

--- src/shaders/sea/vertex.glsl
+++ src/shaders/sea/vertex.glsl
@@ -2,15 +2,22 @@ uniform float uTime;
 
 varying float vElevation;
 
+#include "./LygiaLibrary/genrative/cnoise.glsl"
+
 void main(){
   float uWaveFreqX = 3.5;
   float uWaveFreqZ = 2.0;
   float uWaveElevation = 0.2;
+  float waveBumpyFreq = 3.5;
+  float waveBumpySpeed = 0.5;
+  float waveBumpyElevation = 0.15;
 
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
   float elevation = sin(modelPosition.x * uWaveFreqX + uTime*0.8) *
                     sin(modelPosition.z * uWaveFreqZ + uTime*0.5) *
                     uWaveElevation;
+  
+  elevation += cnoise(vec3(modelPosition.xz * waveBumpyFreq, uTime * waveBumpySpeed)) * waveBumpyElevation;
 
   modelPosition.y += elevation;
 
