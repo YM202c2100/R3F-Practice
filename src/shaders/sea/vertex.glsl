47c029f9085229bdfcb86bfbef3cfcaf6d5b8630
YM202c2100 <y.m202c2100@gmail.com>
2024-07-09 15:25:36 +0900

    [feat] x座標とz座標でy座標が正弦波を描くように変更

--- src/shaders/sea/vertex.glsl
+++ src/shaders/sea/vertex.glsl
@@ -1,5 +1,8 @@
 void main(){
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  modelPosition.y = sin(modelPosition.x * 4.0) *
+                    sin(modelPosition.z * 4.0) *
+                    0.2;
   
   gl_Position = projectionMatrix * viewMatrix * modelPosition;
 }
