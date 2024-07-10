67ea00b4a925dedf4622582b26d885af8ff6a9c6
YM202c2100 <y.m202c2100@gmail.com>
2024-07-10 22:21:10 +0900

    [fix] 色の混ぜる比率を調整

--- src/shaders/sea/fragment.glsl
+++ src/shaders/sea/fragment.glsl
@@ -5,7 +5,12 @@ varying float vElevation;
 
 void main(){
 
-  vec3 color = mix(uDepthColor, uSurfaceColor, vElevation);
+  float mixOffset = 0.1;
+  float mixMultiplier = 0.9;
+
+  float mixStrength = (vElevation + mixOffset) * mixMultiplier;
+
+  vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
   gl_FragColor = vec4(color , 1.0);
   #include <colorspace_fragment>
 }
