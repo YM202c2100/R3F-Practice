abb13caf28064e7999fc76e0f407869c46a1f146
YM202c2100 <y.m202c2100@gmail.com>
2024-07-13 17:16:09 +0900

    [feat] 画面をスクロールできなくなる+今のところOrbitControlsを使う必要がないため削除

--- src/components/MonsterAppearance.jsx
+++ src/components/MonsterAppearance.jsx
@@ -1,11 +1,10 @@
-import { OrbitControls, useGLTF } from "@react-three/drei"
+import { useGLTF } from "@react-three/drei"
 
 
 function MonsterAppearance(){
   const dragonModel = useGLTF("models/dragon/scene.gltf")
 
   return(<>
-    <OrbitControls/>
     <ambientLight intensity={2}/>
     <primitive object={dragonModel.scene} scale={3}/>
   </>)
