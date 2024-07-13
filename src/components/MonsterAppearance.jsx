39c0dab4f94d3cc413ada369a8cae9762668daed
YM202c2100 <y.m202c2100@gmail.com>
2024-07-13 18:06:41 +0900

    [feat]登場アニメーションを一度実行するように変更

--- src/components/MonsterAppearance.jsx
+++ src/components/MonsterAppearance.jsx
@@ -1,8 +1,17 @@
-import { useGLTF } from "@react-three/drei"
-
+import { useAnimations, useGLTF } from "@react-three/drei"
+import { useEffect } from "react"
+import { LoopOnce } from "three"
 
 function MonsterAppearance(){
   const dragonModel = useGLTF("models/dragon/scene.gltf")
+  const animations = useAnimations(dragonModel.animations, dragonModel.scene)
+
+  useEffect(()=>{
+    const action_appear = animations.actions.special
+    action_appear.setLoop(LoopOnce)
+    action_appear.play()
+  },[])
+
 
   return(<>
     <ambientLight intensity={2}/>
