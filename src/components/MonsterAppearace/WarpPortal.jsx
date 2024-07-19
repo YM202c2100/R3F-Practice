1218373b5603bccd41d23de3a0a1a152ef573d6e
YM202c2100 <y.m202c2100@gmail.com>
2024-07-19 15:57:36 +0900

    [style] 全体的にぼやけた感じを出すために、blurを設定

--- src/components/MonsterAppearace/WarpPortal.jsx
+++ src/components/MonsterAppearace/WarpPortal.jsx
@@ -5,7 +5,7 @@ function  WarpPortal(){
         absolute 
         bg-warpPortal 
         landscape:h-[90%] portrait:w-[90%] aspect-square rounded-full
-        shadow-warpPortal"></div>
+        shadow-warpPortal blur-[2px]"></div>
   )
 }
 
