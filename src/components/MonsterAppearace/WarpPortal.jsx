306fa85ffa83c8676b1157e43ec39e955700ef08
YM202c2100 <y.m202c2100@gmail.com>
2024-07-19 16:03:11 +0900

    [style] ワープポータルが回転し続けるように変更

--- src/components/MonsterAppearace/WarpPortal.jsx
+++ src/components/MonsterAppearace/WarpPortal.jsx
@@ -5,7 +5,8 @@ function  WarpPortal(){
         absolute 
         bg-warpPortal 
         landscape:h-[90%] portrait:w-[90%] aspect-square rounded-full
-        shadow-warpPortal blur-[2px]"></div>
+        shadow-warpPortal blur-[2px]
+        animate-warpPortal"></div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ export default {
       },
       boxShadow:{
         warpPortal:"-10px -10px 15px #db2a45, 0px -15px 20px #1e569b, 10px -10px 10px #1c1a34, 15px 0 20px #47ddf4, 10px 10px 20px #1c1a34, 0px 10px 10px #451e72, -11px 10px 20px #1c1a34"
+      },
+      animation:{
+        warpPortal:"spin 2s linear infinite"
       }
     },
   },
